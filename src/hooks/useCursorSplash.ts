a03c32a398b9438d85dac804abc3f99a8d25373b
rahulid <rahul.nsvsr@gmail.com>
2025-07-23 13:58:15 +0530

    Updated useCursorSplash.ts

--- src/hooks/useCursorSplash.ts
+++ src/hooks/useCursorSplash.ts
@@ -1,9 +1,11 @@
 @@ .. @@
      const handleTouch = (e: TouchEvent) => {
+     }
 -      e.preventDefault();
        const touch = e.touches[0] || e.changedTouches[0];
        if (touch) {
          addSplash(touch.clientX, touch.clientY);
+       }
 @@ .. @@
      document.addEventListener('click', handleClick);
 -    document.addEventListener('touchstart', handleTouch, { passive: false });
@@ -11,3 +13,4 @@
  
      return () => {
        document.removeEventListener('click', handleClick);
+     }
